23a6ce0e0e18467f86bd4b477120215a66dc8bec
tushar-kamra <tushar.kamra25@gmail.com>
2020-08-16 14:37:02 +0530

    Commenting system added

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -8,7 +8,43 @@
 
 <hr>
 
+<a href= "<?= base_url(); ?>posts/edit/<?= $post['slug']; ?>" class="btn btn-secondary float-left">Edit</a>
 <?= form_open(base_url() . 'posts/delete/' . $post['id']); ?>
     <button type="submit" class="btn btn-danger">Delete</button>
 </form>
-<a href= "<?= base_url(); ?>posts/edit/<?= $post['slug']; ?>" class="btn btn-default">Edit</a>
+
+<hr>
+
+<h3>Comments</h3>
+<?php if($comments) { ?>
+    <div class="list-group">
+    <?php foreach($comments as $comment): ?>
+        <div class="list-group-item active">
+            <h5><?= $comment['body']; ?> [by <strong><?= $comment['name']; ?></strong>]</h5>
+        </div>
+    <?php endforeach; ?>
+    </div>
+<?php } else { ?>
+    <p>No comments to display.</p>
+<?php } ?>
+
+<hr>
+
+<h3>Add Comments</h3>
+<?= validation_errors(); ?>
+<?= form_open('comments/create/' . $post['id']); ?>
+    <div class="form-group">
+        <label for="name">Name</label>
+        <input type="text" name="name" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="email">Email</label>
+        <input type="text" name="email" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="body">Body</label>
+        <textarea name="body" class="form-control"></textarea>
+    </div>
+    <input type="hidden" name="slug" value="<?= $post['slug']; ?>">
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
